b320666a9d8fea57a059dc85523a45864616db97
glenscheng <122102008+glenscheng@users.noreply.github.com>
2024-03-25 22:13:54 -0400

    fully integrated load cell

--- devkit_v4/AdvancedWebServer/AdvancedWebServer.ino
+++ devkit_v4/AdvancedWebServer/AdvancedWebServer.ino
@@ -1,145 +1,4 @@
-/*
-   Copyright (c) 2015, Majenko Technologies
-   All rights reserved.
-
-   Redistribution and use in source and binary forms, with or without modification,
-   are permitted provided that the following conditions are met:
-
- * * Redistributions of source code must retain the above copyright notice, this
-     list of conditions and the following disclaimer.
-
- * * Redistributions in binary form must reproduce the above copyright notice, this
-     list of conditions and the following disclaimer in the documentation and/or
-     other materials provided with the distribution.
-
- * * Neither the name of Majenko Technologies nor the names of its
-     contributors may be used to endorse or promote products derived from
-     this software without specific prior written permission.
-
-   THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
-   ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
-   WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-   DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
-   ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
-   (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
-   LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
-   ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-   (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
-   SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-*/
-
-/* old code, might be usefule later
-
-// javascript for gettign a value from web page
-    <script>\
-      function getValue() {\
-        var xhttp = new XMLHttpRequest();\
-        xhttp.onreadystatechange = function() {\
-          if (this.readyState == 4 && this.status == 200) {\
-            document.getElementById('displayValue').innerText = this.responseText;\
-          }\
-        };\
-        xhttp.open('GET', '/getvalue', true);\
-        xhttp.send();\
-      }\
-    </script>\
-
-// individual inputs and displays for the month, day, hour, and minute
-    <form action=\"/getmonth\" method=\"get\">\
-      <label for=\"inputValue\">Enter month (1-12):</label>\
-      <input type=\"text\" id=\"inputValue\" name=\"value\">\
-      <input type=\"submit\" value=\"Submit\">\
-    </form>\
-    <form action=\"/getday\" method=\"get\">\
-      <label for=\"inputValue\">Enter day (1-31):</label>\
-      <input type=\"text\" id=\"inputValue\" name=\"value\">\
-      <input type=\"submit\" value=\"Submit\">\
-    </form>\
-    <form action=\"/gethour\" method=\"get\">\
-      <label for=\"inputValue\">Enter hour (0-23):</label>\
-      <input type=\"text\" id=\"inputValue\" name=\"value\">\
-      <input type=\"submit\" value=\"Submit\">\
-    </form>\
-    <form action=\"/getminute\" method=\"get\">\
-      <label for=\"inputValue\">Enter minute (0-59):</label>\
-      <input type=\"text\" id=\"inputValue\" name=\"value\">\
-      <input type=\"submit\" value=\"Submit\">\
-    </form>\
-    <br><br>\
-    <h1>Schedule:</h1>\
-    <label>Month:</label>\
-    <textarea readonly id='displayValue' rows='1' cols='1'>%d</textarea>\
-    <label>Day:</label>\
-    <textarea readonly id='displayValue' rows='1' cols='1'>%d</textarea>\
-    <label>Hour:</label>\
-    <textarea readonly id='displayValue' rows='1' cols='1'>%d</textarea>\
-    <label>Minute:</label>\
-    <textarea readonly id='displayValue' rows='1' cols='1'>%d</textarea>\
-    <br><br>\
-
-// handlers for getting month, day, hour, minute
-    void handleGetMonth() {
-      if (server.hasArg("value") && server.arg("value") != "") {
-        month = server.arg("value").toInt();
-        Serial.print("Received month value: ");
-        Serial.println(month);
-      }
-      // goes back to root web page
-      server.sendHeader("Location", "/", true);  // Set the "Location" header to root URL
-      server.send(302, "text/plain", "");        // Send a 302 Found status code for redirect
-    }
-    void handleGetDay() {
-      if (server.hasArg("value") && server.arg("value") != "") {
-        day = server.arg("value").toInt();
-        Serial.print("Received day value: ");
-        Serial.println(day);
-      }
-      // goes back to root web page
-      server.sendHeader("Location", "/", true);  // Set the "Location" header to root URL
-      server.send(302, "text/plain", "");        // Send a 302 Found status code for redirect
-    }
-    void handleGetHour() {
-      if (server.hasArg("value") && server.arg("value") != "") {
-        hour = server.arg("value").toInt();
-        Serial.print("Received hour value: ");
-        Serial.println(hour);
-      }
-      // server.send(200, "text/plain", "Submitted successfully");
-      // goes back to root web page
-      server.sendHeader("Location", "/", true);  // Set the "Location" header to root URL
-      server.send(302, "text/plain", "");        // Send a 302 Found status code for redirect
-    }
-    void handleGetMinute() {
-      if (server.hasArg("value") && server.arg("value") != "") {
-        minute = server.arg("value").toInt();
-        Serial.print("Received minute value: ");
-        Serial.println(minute);
-      }
-      // goes back to root web page
-      server.sendHeader("Location", "/", true);  // Set the "Location" header to root URL
-      server.send(302, "text/plain", "");        // Send a 302 Found status code for redirect
-    }
-
-// draw graph function that came with the example code
-    void drawGraph() {
-      String out = "";
-      char temp[100];
-      out += "<svg xmlns=\"http://www.w3.org/2000/svg\" version=\"1.1\" width=\"400\" height=\"150\">\n";
-      out += "<rect width=\"400\" height=\"150\" fill=\"rgb(250, 230, 210)\" stroke-width=\"1\" stroke=\"rgb(0, 0, 0)\" />\n";
-      out += "<g stroke=\"black\">\n";
-      int y = rand() % 130;
-      for (int x = 10; x < 390; x += 10) {
-        int y2 = rand() % 130;
-        sprintf(temp, "<line x1=\"%d\" y1=\"%d\" x2=\"%d\" y2=\"%d\" stroke-width=\"1\" />\n", x, 140 - y, x + 10, 140 - y2);
-        out += temp;
-        y = y2;
-      }
-      out += "</g>\n</svg>\n";
-
-      server.send(200, "image/svg+xml", out);
-    }
-
-*/
+// ECE 477 TEAM 3 - SMART PET FEEDER
 
 #include <WiFi.h>
 #include <WiFiClient.h>
@@ -147,10 +6,11 @@
 #include <ESPmDNS.h>
 #include <set>
 #include "time.h"
+#include <Arduino.h>
+#include "HX711.h"
+#include "soc/rtc.h"
 
-const char *ssid = "gern";
-const char *password = "glenpppp0";
-
+// Initialize real time fetching
 const char *ntpServer = "pool.ntp.org";
 const long gmtOffset_sec = -14400; // EST: GMT -04:00 (-14400 s)
 const int daylightOffset_sec = 0;
@@ -162,13 +22,18 @@ char timeID[3] = "";
 int idDay = -1;
 int idHour = -1;
 
+// Initialize LED pins
 #define LED_PIN 4
+#define INT_LED_PIN1 22 // other one is IO23 (solder connection was bad as of 3/25/24)
 
+// Initialize web server
+const char *ssid = "gern";
+const char *password = "glenpppp0";
 WebServer server(80);
 
+// Initialize scheduling system on web page
 using namespace std;
 set<String> schedule;
-
 char *checked[10][7];
 // initialize all checkboxes to be empty
 void init_checked() {
@@ -177,6 +42,19 @@ void init_checked() {
       checked[i][j] = "";
     }
   }
+}
+
+// Initialize load cell
+#define LOADCELL_DOUT_PIN 19
+#define LOADCELL_SCK_PIN 18
+#define CALIBRATION_FACT 202.86
+HX711 scale;
+float bowlWeight = -1;
+void init_scale() {
+  // Serial.println("Initializing the scale");
+  scale.begin(LOADCELL_DOUT_PIN, LOADCELL_SCK_PIN);
+  scale.set_scale(CALIBRATION_FACT);
+  scale.tare();
 }
 
 
@@ -198,6 +76,8 @@ void handleRoot() {
     <body>\
       <h1>Pet Feeder Menu</h1>\
       <a href='http://172.20.10.8/'> Link to video stream! </a>\
+      <br><br>\
+      Weight of food in bowl: %f grams.\
       <br><br>\
       <table>\
         <tbody>\
@@ -525,16 +405,17 @@ void handleRoot() {
   </html>";
 
   // Serial.println(strlen(html));
-  snprintf(temp, html_size, html, checked[0][0], checked[0][1], checked[0][2], checked[0][3], checked[0][4], checked[0][5], checked[0][6],
-                                  checked[1][0], checked[1][1], checked[1][2], checked[1][3], checked[1][4], checked[1][5], checked[1][6],
-                                  checked[2][0], checked[2][1], checked[2][2], checked[2][3], checked[2][4], checked[2][5], checked[2][6],
-                                  checked[3][0], checked[3][1], checked[3][2], checked[3][3], checked[3][4], checked[3][5], checked[3][6],
-                                  checked[4][0], checked[4][1], checked[4][2], checked[4][3], checked[4][4], checked[4][5], checked[4][6],
-                                  checked[5][0], checked[5][1], checked[5][2], checked[5][3], checked[5][4], checked[5][5], checked[5][6],
-                                  checked[6][0], checked[6][1], checked[6][2], checked[6][3], checked[6][4], checked[6][5], checked[6][6],
-                                  checked[7][0], checked[7][1], checked[7][2], checked[7][3], checked[7][4], checked[7][5], checked[7][6],
-                                  checked[8][0], checked[8][1], checked[8][2], checked[8][3], checked[8][4], checked[8][5], checked[8][6],
-                                  checked[9][0], checked[9][1], checked[9][2], checked[9][3], checked[9][4], checked[9][5], checked[9][6]);
+  snprintf(temp, html_size, html, bowlWeight, 
+            checked[0][0], checked[0][1], checked[0][2], checked[0][3], checked[0][4], checked[0][5], checked[0][6],
+            checked[1][0], checked[1][1], checked[1][2], checked[1][3], checked[1][4], checked[1][5], checked[1][6],
+            checked[2][0], checked[2][1], checked[2][2], checked[2][3], checked[2][4], checked[2][5], checked[2][6],
+            checked[3][0], checked[3][1], checked[3][2], checked[3][3], checked[3][4], checked[3][5], checked[3][6],
+            checked[4][0], checked[4][1], checked[4][2], checked[4][3], checked[4][4], checked[4][5], checked[4][6],
+            checked[5][0], checked[5][1], checked[5][2], checked[5][3], checked[5][4], checked[5][5], checked[5][6],
+            checked[6][0], checked[6][1], checked[6][2], checked[6][3], checked[6][4], checked[6][5], checked[6][6],
+            checked[7][0], checked[7][1], checked[7][2], checked[7][3], checked[7][4], checked[7][5], checked[7][6],
+            checked[8][0], checked[8][1], checked[8][2], checked[8][3], checked[8][4], checked[8][5], checked[8][6],
+            checked[9][0], checked[9][1], checked[9][2], checked[9][3], checked[9][4], checked[9][5], checked[9][6]);
   server.send(200, "text/html", temp);
   free(temp);
 }
@@ -671,14 +552,27 @@ void handleMotor() {
   }
 }
 
+void handleScale() {
+  scale.power_up();
+
+  bowlWeight = scale.get_units();
+  Serial.println("brendab sucks");
+  Serial.println(bowlWeight);
+
+  scale.power_down();
+}
+
 
 
 void setup(void) {
   init_checked();
+  init_scale();
   configTime(gmtOffset_sec, daylightOffset_sec, ntpServer);
 
   pinMode(LED_PIN, OUTPUT); // Set the pin as output
   digitalWrite(LED_PIN, LOW); // Turn off LED
+  pinMode(INT_LED_PIN1, OUTPUT); // Set the pin as output
+  digitalWrite(INT_LED_PIN1, LOW); // Turn off LED
 
   Serial.begin(115200);
   WiFi.mode(WIFI_STA);
@@ -711,14 +605,19 @@ void setup(void) {
 
   server.begin();
   Serial.println("HTTP server started");
+  digitalWrite(INT_LED_PIN1, HIGH); // Turn on LED to indicate web server started
 }
 
 
-
+int i = 0;
 void loop(void) {
   server.handleClient();
   if (!schedule.empty()) {
     handleMotor();
   }
+  if (i % 10 == 0) { // 10 * ~100 ms delay
+    handleScale();
+  }
   delay(100); // allow the cpu to switch to other tasks
+  i++;
 }
