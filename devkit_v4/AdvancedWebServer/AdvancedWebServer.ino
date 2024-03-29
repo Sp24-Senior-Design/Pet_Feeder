65de468bf22058d024b419bd098544565c0fdf09
glenscheng <122102008+glenscheng@users.noreply.github.com>
2024-03-29 16:49:57 -0400

    all integrated

--- devkit_v4/AdvancedWebServer/AdvancedWebServer.ino
+++ devkit_v4/AdvancedWebServer/AdvancedWebServer.ino
@@ -23,7 +23,7 @@ int idDay = -1;
 int idHour = -1;
 
 // Initialize LED pins
-#define LED_PIN 4
+// #define LED_PIN 4
 #define INT_LED_PIN1 22 // other one is IO23 (solder connection was bad as of 3/25/24)
 
 // Initialize web server
@@ -57,6 +57,166 @@ void init_scale() {
   scale.tare();
 }
 
+// Initialize motor
+#define dirPin 27
+#define stepPin 33
+#define nreset 26
+#define nsleep 32
+#define stepsPerRevolution 200
+void init_motor() {
+  // Declare pins as output:
+  pinMode(stepPin, OUTPUT);
+  pinMode(dirPin, OUTPUT);
+  pinMode(nreset, OUTPUT);
+  pinMode(nsleep, OUTPUT);
+}
+
+// Initialize IR sensor
+uint16_t containerDist = -1;
+#define TF_ADDR 0x10 // Default TF-LUNA address
+// - - - -   Register Names and Numbers   - - - -
+#define TFL_DIST_LO          0x00  //R Unit: cm
+#define TFL_DIST_HI          0x01  //R
+#define TFL_FLUX_LO          0x02  //R
+#define TFL_FLUX_HI          0x03  //R
+#define TFL_TEMP_LO          0x04  //R Unit: 0.01 Celsius
+#define TFL_TEMP_HI          0x05  //R
+#define TFL_TICK_LO          0x06  //R Timestamp
+#define TFL_TICK_HI          0x07  //R
+#define TFL_ERR_LO           0x08  //R
+#define TFL_ERR_HI           0x09  //R
+#define TFL_VER_REV          0x0A  //R
+#define TFL_VER_MIN          0x0B  //R
+#define TFL_VER_MAJ          0x0C  //R
+#define TFL_SAVE_SETTINGS    0x20  //W -- Write 0x01 to save
+#define TFL_SOFT_RESET       0x21  //W -- Write 0x02 to reboot.
+                       // Lidar not accessible during few seconds,
+                       // then register value resets automatically
+#define TFL_SET_I2C_ADDR     0x22  //W/R -- Range 0x08,0x77.
+                       // Must reboot to take effect.
+#define TFL_SET_TRIG_MODE    0x23  //W/R -- 0-continuous, 1-trigger
+#define TFL_TRIGGER          0x24  //W  --  1-trigger once
+#define TFL_DISABLE          0x25  //W/R -- 0-disable, 1-enable
+#define TFL_FPS_LO           0x26  //W/R -- lo byte
+#define TFL_FPS_HI           0x27  //W/R -- hi byte
+#define TFL_SET_LO_PWR       0x28  //W/R -- 0-normal, 1-low power
+#define TFL_HARD_RESET       0x29  //W  --  1-restore factory settings
+// - - - -                                - - - -
+// 1:1000 10:100 (bps)
+#define TIME_WAIT 5 // Frame Rate
+#define SDA_PIN 14 // IO14
+#define SCL_PIN 4 // IO12, NOPE IO4
+#define SDA_IN pinMode(SDA_PIN, INPUT)
+#define SDA_OUT pinMode(SDA_PIN, OUTPUT)
+#define SDA_HIGH digitalWrite(SDA_PIN, HIGH)
+#define SDA_LOW digitalWrite(SDA_PIN, LOW)
+#define SCL_HIGH digitalWrite(SCL_PIN, HIGH)
+#define SCL_LOW digitalWrite(SCL_PIN, LOW)
+#define WAIT_DLY delay(TIME_WAIT);
+void init_pins() {
+  pinMode(SDA_PIN, OUTPUT);
+  pinMode(SCL_PIN, OUTPUT);
+}
+void i2c_start() {
+  SDA_HIGH;
+  WAIT_DLY;
+  SCL_HIGH;
+  WAIT_DLY;
+  SDA_LOW;
+  WAIT_DLY;
+  SCL_LOW;
+  WAIT_DLY;
+}
+void i2c_write_addr() {
+  // iterate through each bit to write a 1 or a 0
+  for (uint8_t idx = 64; idx > 0; idx /= 2)
+  {
+    i2c_write_bit(TF_ADDR & idx);
+  }
+}
+void i2c_write_bit(int value)
+{
+  (value != 0) ? SDA_HIGH : SDA_LOW;
+  WAIT_DLY;
+  SCL_HIGH;
+  WAIT_DLY;
+  SCL_LOW;
+  WAIT_DLY;
+}
+uint8_t i2c_read_bit() {
+
+  uint8_t bit_val = 0;
+  SDA_IN;
+
+  SCL_HIGH;
+  WAIT_DLY;
+  bit_val |= digitalRead(SDA_PIN);
+  WAIT_DLY;
+  SCL_LOW;
+  WAIT_DLY;
+
+  SDA_OUT;
+
+  return bit_val;
+}
+void i2c_stop() {
+  SDA_LOW;
+  WAIT_DLY;
+  SCL_HIGH;
+  WAIT_DLY; 
+  SDA_HIGH;
+  WAIT_DLY;
+}
+void i2c_write_byte(uint8_t value) {
+  // iterate through each bit to write a 1 or a 0
+  for (uint8_t idx = 128; idx > 0; idx /= 2)
+  {
+    i2c_write_bit(value & idx);
+  }
+}
+uint8_t i2c_read_byte() {
+  uint8_t value = 0;
+
+  // iterate through each bit to write a 1 or a 0
+  for (uint8_t idx = 0; idx < 8; idx++)
+  {
+    uint8_t digit = i2c_read_bit();
+    
+    value |= digit;
+    value = value << 1;
+    // Serial.print("Current IDX: ");
+    // Serial.print((idx));
+    // Serial.print("BIT: ");
+    // Serial.print((digit));
+    // Serial.print("Value: ");
+    // Serial.println((value));
+  }
+  // Serial.println("BYTE");
+  i2c_write_bit(0); // ACK
+  return value;
+}
+void init_i2c(int reg)
+{
+  // Serial.println("I2C Bit-Banging");
+  init_pins();
+  // Intialize Communication
+  i2c_start();
+  i2c_write_addr();
+  i2c_write_bit(0);
+
+  bool ack = i2c_read_bit();
+  // Serial.print("ACK Check: ");
+  // Serial.print(ack);
+  // Serial.print("\n");
+
+  i2c_write_byte(reg);
+  ack = i2c_read_bit();
+  // Serial.print("ACK Check: ");
+  // Serial.print(ack);
+  // Serial.print("\n");
+  i2c_stop();
+}
+
 
 void handleRoot() {
   int html_size = 2000 * 10;
@@ -78,6 +238,7 @@ void handleRoot() {
       <a href='http://172.20.10.8/'> Link to video stream! </a>\
       <br><br>\
       Weight of food in bowl: %f grams.\
+      Distance of food in container: %d cm.\
       <br><br>\
       <table>\
         <tbody>\
@@ -405,7 +566,7 @@ void handleRoot() {
   </html>";
 
   // Serial.println(strlen(html));
-  snprintf(temp, html_size, html, bowlWeight, 
+  snprintf(temp, html_size, html, bowlWeight, containerDist,
             checked[0][0], checked[0][1], checked[0][2], checked[0][3], checked[0][4], checked[0][5], checked[0][6],
             checked[1][0], checked[1][1], checked[1][2], checked[1][3], checked[1][4], checked[1][5], checked[1][6],
             checked[2][0], checked[2][1], checked[2][2], checked[2][3], checked[2][4], checked[2][5], checked[2][6],
@@ -494,8 +655,8 @@ void getTime() {
     idDay = 6;
   }
 
-  // Serial.print("timeHour: ");
-  // Serial.println(timeHour);
+  Serial.print("timeHour: ");
+  Serial.println(timeHour);
   if (strcmp(timeHour, "09") == 0) {
     idHour = 0;
   } else if (strcmp(timeHour, "10") == 0) {
@@ -521,11 +682,11 @@ void getTime() {
   }
   
   // print id from time
-  Serial.print("idHour idDay: ");
-  Serial.print(idHour);
-  Serial.print(" ");
-  Serial.println(idDay);
-  Serial.println();
+  // Serial.print("idHour idDay: ");
+  // Serial.print(idHour);
+  // Serial.print(" ");
+  // Serial.println(idDay);
+  // Serial.println();
 
   // print time
   // Serial.println(&timeinfo, "%A, %B %d %Y %H:%M:%S");
@@ -541,14 +702,31 @@ void handleMotor() {
   idFromTime[2] = '\0';
 
   if (schedule.count(idFromTime)) {
-    // flash LED (gpio pin)
-    digitalWrite(LED_PIN, HIGH); // Turn on LED
-    delay(1000);
-    digitalWrite(LED_PIN, LOW); // Turn off LED
+    // Set the spinning direction clockwise:
+    digitalWrite(dirPin, HIGH);
+    digitalWrite(nreset, HIGH);
+    digitalWrite(nsleep, HIGH);
+
+    // Spin the stepper motor 1 revolution:
+    for (int i = 0; i < stepsPerRevolution*0.6; i++) {
+        // These four lines result in 1 step:
+        digitalWrite(stepPin, HIGH);
+        delayMicroseconds(5000);
+        digitalWrite(stepPin, LOW);
+        delayMicroseconds(5000);
+      }
+
+    // // flash LED (gpio pin)
+    // digitalWrite(LED_PIN, HIGH); // Turn on LED
+    // delay(1000);
+    // digitalWrite(LED_PIN, LOW); // Turn off LED
 
     // remove id from schedule
     schedule.erase(idFromTime);
     checked[idHour][idDay] = ""; // uncheck box
+
+    digitalWrite(nreset, LOW);
+    digitalWrite(nsleep, LOW);
   }
 }
 
@@ -556,21 +734,50 @@ void handleScale() {
   scale.power_up();
 
   bowlWeight = scale.get_units();
-  Serial.println("brendab sucks");
-  Serial.println(bowlWeight);
+  // Serial.println("brendab sucks");
+  // Serial.println(bowlWeight);
 
   scale.power_down();
+}
+
+void handleIR()
+{
+  init_i2c(TFL_DIST_LO);
+  i2c_start();
+  i2c_write_addr();
+  i2c_write_bit(1); // Read bit
+  uint8_t low_bits = i2c_read_byte();
+
+  i2c_stop();
+  
+  init_i2c(TFL_DIST_HI);
+  i2c_start();
+  i2c_write_addr();
+  i2c_write_bit(1); // Read bit
+  uint8_t upper_bits = i2c_read_byte();
+  i2c_stop();
+
+  uint16_t dist_val = upper_bits << 8;
+  dist_val |= low_bits;
+  // Serial.print("Lower: ");
+  // Serial.println(low_bits);
+  // Serial.print("Upper: ");
+  // Serial.println(upper_bits);
+  // Serial.print("Final: ");
+  // Serial.println(dist_val);
+  containerDist = dist_val;
 }
 
 
 
 void setup(void) {
   init_checked();
-  init_scale();
+  // init_scale();
+  init_motor();
   configTime(gmtOffset_sec, daylightOffset_sec, ntpServer);
 
-  pinMode(LED_PIN, OUTPUT); // Set the pin as output
-  digitalWrite(LED_PIN, LOW); // Turn off LED
+  // pinMode(LED_PIN, OUTPUT); // Set the pin as output
+  // digitalWrite(LED_PIN, LOW); // Turn off LED
   pinMode(INT_LED_PIN1, OUTPUT); // Set the pin as output
   digitalWrite(INT_LED_PIN1, LOW); // Turn off LED
 
@@ -616,8 +823,10 @@ void loop(void) {
     handleMotor();
   }
   if (i % 10 == 0) { // 10 * ~100 ms delay
-    handleScale();
+    // handleScale();
+    handleIR();
   }
+  
   delay(100); // allow the cpu to switch to other tasks
   i++;
 }
